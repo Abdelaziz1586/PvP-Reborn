8aede309c2181842c6280730223ae5b501d53523
Abdelaziz1586 <gamingblueslurp@gmail.com>
2023-06-30 19:01:47 +0300

    Update

---
+++ src/main/java/me/pebbleprojects/pvpreborn/handlers/QuestsHandler.java
@@ -0,0 +1,15 @@
+package me.pebbleprojects.pvpreborn.handlers;
+
+public class QuestsHandler {
+
+    // TODO: Finish daily quests
+
+    private final Handler handler;
+
+    public QuestsHandler(final Handler handler) {
+        this.handler = handler;
+    }
+
+
+
+}
